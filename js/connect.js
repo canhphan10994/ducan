b5e16e079329c5a9947f752f9a7a01dbf8f1f4a1
canhphan10994 <50878552+canhphan10994@users.noreply.github.com>
2019-05-27 22:28:08 +0700

    Update connect.js
    connect update 2

--- js/connect.js
+++ js/connect.js
@@ -8,29 +8,29 @@ $(document).ready(function() {
   // toggle menu
   let menuList = $('.toggle__list li');
   for (let item of menuList) {
-    $(item.firstElementChild).attr('href', '../html/productlist.html');
+    $(item.firstElementChild).attr('href', 'productlist.html');
   }
 
   // NAVBAR
   let navBar = $('#navbar ul li');
   for (let item of navBar) {
     if (item.innerText == 'Giới thiệu') {
-      $(item.firstElementChild).attr('href', 'html/reviews.html');
+      $(item.firstElementChild).attr('href', 'reviews.html');
     }
     if (item.innerText == 'Tin tức') {
-      $(item.firstElementChild).attr('href', 'html/news.html');
+      $(item.firstElementChild).attr('href', 'news.html');
     }
     if (item.innerText == 'Khuyến mãi') {
-      $(item.firstElementChild).attr('href', 'html/news.html');
+      $(item.firstElementChild).attr('href', 'news.html');
     }
     if (item.innerText == 'Liên Hệ') {
-      $(item.firstElementChild).attr('href', 'html/contact.html');
+      $(item.firstElementChild).attr('href', 'contact.html');
     }
   }
 
   // Product
   $('.product').click( () =>{
-    window.location.replace('http://127.0.0.1:8080/html/product.html');
+    window.location.replace('https://canhphan10994.github.io/ducans/html/product.html');
   });
 
 });
